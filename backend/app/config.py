df8e30d2871e5db501332454307a15629377dc55
Sanjeev Reddy <sanjeevreddysuram2003@gmail.com>
2025-03-05 22:30:43 +0530

    changed database url to deploy

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,7 +2,7 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost:5432/postgres")
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://neondb_owner:npg_Po57hbnpUKYf@ep-plain-unit-a8zun7cf-pooler.eastus2.azure.neon.tech/neondb?sslmode=require")
     SECRET_KEY: str = os.getenv("SECRET_KEY", "sanju")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
